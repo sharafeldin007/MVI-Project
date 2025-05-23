3c1e760cc409b3216f3a9738406a76ecff0ba8a6
Sharaf <sharafa839@gmail.com>
2025-05-23 16:18:04 +0300

    Initial Commit

---
+++ MVIProject/MVIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVIProjectApp.swift
+//  MVIProject
+//
+//  Created by Sharaf on 5/23/25.
+//
+
+import SwiftUI
+
+@main
+struct MVIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MVIProjectTests/MVIProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  MVIProjectTests.swift
+//  MVIProjectTests
+//
+//  Created by Sharaf on 5/23/25.
+//
+
+import Testing
+@testable import MVIProject
+
+struct MVIProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
